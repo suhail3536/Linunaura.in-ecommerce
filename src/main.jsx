7de0ca63ed7d60013dcad5e7fcf685d8b245a2bc
Suhail <shab35360@gmail.com>
2026-05-05 22:44:47 +0530

    fix build errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
 
-import App from './App.jsx';
 import AdminApp from './Admin.jsx';
+import App from './App.jsx';
 
 function Router() {
-  const path = window.location.pathname;
-
-  if (path === '/admin') {
+  if (window.location.pathname === '/admin') {
     return <AdminApp />;
   }
-
   return <App />;
 }
 
